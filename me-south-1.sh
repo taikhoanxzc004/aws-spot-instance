7261c1dd92c9cd33af2b7a365d5b02c6ea48097b
kalinh <taikhoanxzc004@gmail.com>
2025-02-28 18:32:44 +0700

    Add files via upload

--- me-south-1.sh
+++ me-south-1.sh
@@ -4,4 +4,4 @@ aws ec2 authorize-security-group-ingress --group-name Group --protocol tcp --por
 aws ec2 authorize-security-group-ingress --group-name Group --protocol tcp --port 0-65535 --cidr 0.0.0.0/0 --region me-south-1
 aws ec2 authorize-security-group-ingress --group-name Group --protocol udp --port 0-65535 --cidr 0.0.0.0/0 --region me-south-1
 aws ec2 create-key-pair --key-name KeyPair --region me-south-1
-aws ec2 run-instances --instance-market-options 'MarketType=spot' --image-id ami-0ad87b81ce7e181a1 --key-name KeyPair --security-groups Group --instance-type c6g.medium --min-size 5 --max-size 5 --desired-capacity 5 --block-device-mappings 'DeviceName=/dev/sda1,Ebs={VolumeSize=100}' --user-data fileb:///root/AWS-ARM-internetincome.sh --region me-south-1
+aws ec2 run-instances --instance-market-options 'MarketType=spot,SpotOptions={MaxPrice="100"}' --count 5 --instance-market-options 'MarketType=spot' --image-id ami-0ad87b81ce7e181a1 --key-name KeyPair --security-groups Group --instance-type c6g.medium  --block-device-mappings 'DeviceName=/dev/sda1,Ebs={VolumeSize=100}' --user-data fileb:///root/AWS-ARM-internetincome.sh --region me-south-1
